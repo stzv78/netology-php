e5f88dd1647408fb1655e4b5bbc200deaa60ff9c
Zoya Stepcheva <stzv78@mail.ru>
2017-03-19 09:44:49 +0300

    first commit

---
+++ MyTable.php
@@ -0,0 +1,44 @@
+<?php
+header('Content-type: text/html; charset=utf-8');
+$json_data = file_get_contents('http://university.netology.ru/u/stepcheva/data.json');
+//http://university.netology.ru/u/stepcheva/data.json
+$data = mb_convert_encoding($json_data,"UTF-8");
+$data = json_decode($data,true);
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Телефонная книга</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+</head>
+
+<body>
+<div class="container">
+    <table class="table table-bordered table-striped">
+        <thead>
+            <tr class="danger">
+                <th>Имя</th>
+                <th>Фамилия</th>
+                <th>Адрес</th>
+                <th>Телефон</th>
+            </tr>
+       </thead>
+
+        <tbody>
+        <?php 
+            foreach ($data as $key => $person) 
+            {
+                echo '<tr>';
+                foreach ($person as $value)
+                {
+                    echo "<td> $value </td>";
+                }
+                echo '</tr>';
+            }
+        ?>
+        </tbody>
+    </table>
+</div>
+</body>
+</html>
