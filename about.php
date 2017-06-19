e6a074e25615e4c3786c672dc1e29ea1821acc22
stzv78 <stzv78@mail.ru>
2017-06-19 19:01:38 +0300

    first commit

--- about.php
+++ about.php
@@ -1,18 +1,18 @@
 <?php
-$name="Зоя";
-$surname="Степчева";
-$age=38;
-$mail='stzv78@gmail.com';
-$city='Ульяновск';
-$about='Технический писатель в АИС "Город"';
+$name = "Зоя";
+$surname = "Степчева";
+$age = 38;
+$mail = 'stzv78@gmail.com';
+$city = 'Ульяновск';
+$about = 'Технический писатель в АИС "Город"';
 ?>
 <!Doctype html>
 <!DOCTYPE html>
 <html>
 <head>
-	<title><?=$surname.' '.$name; ?></title>
-	<meta charset="utf-8">
-	<style type="text/css">
+	<title><?= $surname . ' ' . $name; ?></title>
+	<meta charset = "utf-8">
+	<style type = "text/css">
 
         body {
             font-family: sans-serif;  
@@ -33,23 +33,23 @@ $about='Технический писатель в АИС "Город"';
 	<h1>Страница пользователя <?=$surname.' '.$name; ?></h1>
         <dl>
             <dt>Имя</dt>
-            <dd><?=$name; ?></dd>
+            <dd><?= $name; ?></dd>
         </dl>
         <dl>
             <dt>Возраст</dt>
-            <dd><?=$age; ?></dd>
+            <dd><?= $age; ?></dd>
         </dl>
         <dl>
             <dt>Адрес электронной почты</dt>
-            <dd><?php echo '<a href=/'.$mail.'/">'.$mail.'</a>'; ?></dd>
+            <dd><?php echo '<a href=/'. $mail . '/">' . $mail . '</a>'; ?></dd>
         </dl>
         <dl>
             <dt>Город</dt>
-            <dd><?=$city; ?></dd>
+            <dd><?= $city; ?></dd>
         </dl>
         <dl>
             <dt>О себе</dt>
-            <dd><?=$about; ?></dd>
+            <dd><?= $about; ?></dd>
         </dl>
 	
 </body>
